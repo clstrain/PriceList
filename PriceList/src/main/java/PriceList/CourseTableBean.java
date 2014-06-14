0b2a1103b7dd77c2a939d02778e81c262bca7305
Armando <aperea@ciena.com>
2014-06-13 21:46:30 -0400

    fixed datatable way better

--- PriceList/src/main/java/PriceList/CourseTableBean.java
+++ PriceList/src/main/java/PriceList/CourseTableBean.java
@@ -7,26 +7,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.bean.ViewScoped;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
 import org.primefaces.model.SelectableDataModel;
 
 @ManagedBean(name = "courseList")
-@SessionScoped
+@ViewScoped
 
 public class CourseTableBean extends ListDataModel<CourseListing>
         implements SelectableDataModel<CourseListing>, Serializable {
 
     private List<CourseListing> courses, filteredCourses, shoppingCartList;
     public static Integer CP_ID;
-    public static String number, product, name, location, unit, duration,
-            type, role, category, price, maxNumStudents,
-            superProduct, subProduct;
-    private String response, selectedCategory="", selectedType="";
+    
+    private String response;
     public CourseListing selectedCourse, selectedShoppingcartCourse;
 
     private List<SelectItem> locationSelectItems = new ArrayList<SelectItem>(),
@@ -34,11 +30,7 @@ public class CourseTableBean extends ListDataModel<CourseListing>
             roleSelectItems = new ArrayList<SelectItem>(),
             categorySelectItems = new ArrayList<SelectItem>(),
             productSelectItems = new ArrayList<SelectItem>();
-
-    private List<SelectItem> toptypeSelectItems = new ArrayList<SelectItem>(),
-            topcategorySelectItems = new ArrayList<SelectItem>();
     
-
     CourseDAO courseDAO = new CourseDAO();
 
     @PostConstruct
@@ -62,87 +54,11 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         courses = courseDAO.getAll();
 
         response = courseDAO.response;
-        
-        selectedCategory="";
-        selectedType="";
-
-        //load the dropdown lists into HashSets in order to remove dupes 
-        Set<String> categories = new HashSet<String>();
-        Set<String> types = new HashSet<String>();
-
-        for (CourseListing courseListing : courses) {
-            categories.add(courseListing.getCategory());
-            types.add(courseListing.getType());
-        }
-        
-        //setup selectItems
-        toptypeSelectItems = new ArrayList<SelectItem>();
-        topcategorySelectItems = new ArrayList<SelectItem>();
-        productSelectItems = new ArrayList<SelectItem>();
-
-        toptypeSelectItems.add(new SelectItem("", "Select One"));
-        topcategorySelectItems.add(new SelectItem("", "Select One"));
-        productSelectItems.add(new SelectItem("", "Select One"));
-
-        List<String>products = new ArrayList<String>(courseDAO.getmenuItemsProduct());
-
-        //place strings in to SelectItems
-        for (String stringIterator : types) {
-            if (!stringIterator.isEmpty()) {
-                toptypeSelectItems.add(new SelectItem(stringIterator));
-            }
-        }
-
-        for (String stringIterator : categories) {
-            if (!stringIterator.isEmpty()) {
-                topcategorySelectItems.add(new SelectItem(stringIterator));
-            }
-        } 
-        
-        for (String stringIterator : products) {
-            if (!stringIterator.isEmpty()) {
-                productSelectItems.add(new SelectItem(stringIterator));
-            }
-        }
-
-        //load products menu
+               
                 
         LoadDataTableMenus();
     }
-
-    public void updateCourseList() {
-        courses.clear();
-
-        if ("".equals(selectedCategory) && !"".equals(selectedType)) {
-            courses = courseDAO.getByType(selectedType);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Updated", "Showing courses for type " + selectedType));
-        } else if ("".equals(selectedType) && !"".equals(selectedCategory)) {
-            courses = courseDAO.getByCategory(selectedCategory);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Updated", "Showing courses for category " + selectedCategory));
-        } else if ("".equals(selectedType) && "".equals(selectedCategory)) {
-            courses = courseDAO.getAll();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Updated", "Showing all courses"));
-        } else if (!"".equals(selectedType) && !"".equals(selectedCategory)) {
-            courses = courseDAO.getByCategoryAndType(selectedCategory, selectedType);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Updated", "Showing courses for category " + selectedCategory + " and type "+selectedType));
-        }
-
-        //redo the datatable menus
-        LoadDataTableMenus();
-    }
-
-    public void showAllCategories() {
-        System.out.println(" derp " + selectedCategory);
-    }
-
-    public void processProductChange() {
-        this.setSuperProduct(null);
-    }
-
-    public void processSuperProductChange() {
-        this.setproduct(null);
-    }
-
+      
     public void addToCart() {
         shoppingCartList.add(selectedCourse);
 
@@ -176,94 +92,6 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         CourseTableBean.CP_ID = C_ID;
     }
 
-    public String getnumber() {
-        return number;
-    }
-
-    public void setnumber(String number) {
-        CourseTableBean.number = number;
-    }
-
-    public String getproduct() {
-        return product;
-    }
-
-    public void setproduct(String product) {
-        CourseTableBean.product = product;
-    }
-
-    public String getname() {
-        return name;
-    }
-
-    public void setname(String name) {
-        CourseTableBean.name = name;
-    }
-
-    public String getlocation() {
-        return location;
-    }
-
-    public void setlocation(String location) {
-        CourseTableBean.location = location;
-    }
-
-    public String getunit() {
-        return unit;
-    }
-
-    public void setunit(String unit) {
-        CourseTableBean.unit = unit;
-    }
-
-    public String getduration() {
-        return duration;
-    }
-
-    public void setduration(String duration) {
-        CourseTableBean.duration = duration;
-    }
-
-    public String gettype() {
-        return type;
-    }
-
-    public void settype(String type) {
-        CourseTableBean.type = type;
-    }
-
-    public String getrole() {
-        return role;
-    }
-
-    public void setrole(String role) {
-        CourseTableBean.role = role;
-    }
-
-    public String getcategory() {
-        return category;
-    }
-
-    public void setcategory(String category) {
-        CourseTableBean.category = category;
-    }
-
-    public String getprice() {
-        return price;
-    }
-
-    public void setprice(String price) {
-        CourseTableBean.price = price;
-    }
-
-    public String getmaxNumStudents() {
-        return maxNumStudents;
-    }
-
-    public void setmaxNumStudents(String maxNumStudents) {
-        CourseTableBean.maxNumStudents = maxNumStudents;
-    }
-
     public List<CourseListing> getCourses() {
 
         return courses;
@@ -290,21 +118,6 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         this.filteredCourses = filteredCourses;
     }
 
-    public String getSuperProduct() {
-        return superProduct;
-    }
-
-    public void setSuperProduct(String superProduct) {
-        CourseTableBean.superProduct = superProduct;
-    }
-
-    public String getSubProduct() {
-        return subProduct;
-    }
-
-    public void setSubProduct(String subProduct) {
-        CourseTableBean.subProduct = subProduct;
-    }
 
     public List<SelectItem> getLocationSelectItems() {
         return locationSelectItems;
@@ -362,38 +175,6 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         this.categorySelectItems = categorySelectItems;
     }
 
-    public String getSelectedCategory() {
-        return selectedCategory;
-    }
-
-    public void setSelectedCategory(String selectedCategory) {
-        this.selectedCategory = selectedCategory;
-    }
-
-    public String getSelectedType() {
-        return selectedType;
-    }
-
-    public void setSelectedType(String selectedType) {
-        this.selectedType = selectedType;
-    }
-
-    public List<SelectItem> getToptypeSelectItems() {
-        return toptypeSelectItems;
-    }
-
-    public void setToptypeSelectItems(List<SelectItem> toptypeSelectItems) {
-        this.toptypeSelectItems = toptypeSelectItems;
-    }
-
-    public List<SelectItem> getTopcategorySelectItems() {
-        return topcategorySelectItems;
-    }
-
-    public void setTopcategorySelectItems(List<SelectItem> topcategorySelectItems) {
-        this.topcategorySelectItems = topcategorySelectItems;
-    }
-
     public List<SelectItem> getProductSelectItems() {
         return productSelectItems;
     }
@@ -402,10 +183,19 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         this.productSelectItems = productSelectItems;
     }
     
-    
-
     private void LoadDataTableMenus() {
 
+        
+        productSelectItems = new ArrayList<SelectItem>();
+       
+        List<String>products = new ArrayList<String>(courseDAO.getmenuItemsProduct());
+                     
+        for (String stringIterator : products) {
+            if (!stringIterator.isEmpty()) {
+                productSelectItems.add(new SelectItem(stringIterator));
+            }
+        }
+        
         //load the dropdown lists into HashSets in order to remove dupes 
         Set<String> locations = new HashSet<String>();
         Set<String> categories = new HashSet<String>();
@@ -435,12 +225,6 @@ public class CourseTableBean extends ListDataModel<CourseListing>
         typeSelectItems = new ArrayList<SelectItem>();
         roleSelectItems = new ArrayList<SelectItem>();
         categorySelectItems = new ArrayList<SelectItem>();
-
-        
-        locationSelectItems.add(new SelectItem("", "Select One"));
-        typeSelectItems.add(new SelectItem("", "Select One"));
-        roleSelectItems.add(new SelectItem("", "Select One"));
-        categorySelectItems.add(new SelectItem("", "Select One"));
 
         //place strings in to SelectItems
         for (String stringIterator : locationsArrayList) {
